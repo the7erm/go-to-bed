08d8514c3dc7de346add6f2d119a6a841dcabceb
Eugene R. Miller <theerm@gmail.com>
2013-07-03 00:13:42 -0600

    created update script

---
+++ python/update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+git pull
+sudo cp -v ./go-to-bed-daemon.py /usr/bin/
+sudo cp -v ./go-to-bed.py /usr/bin/
+sudo cp -v ./go-to-bed-init.sh /etc/init.d/go-to-bed
+
+CONFIG_FILE="/etc/go-to-bed.conf"
+
+if [ ! -f "$CONFIG_FILE" ]
+then
+    sudo echo "USERS=\"\"" > $CONFIG_FILE
+    sudo echo "URL=\"http://localhost/go-to-bed/\"\n" >> $CONFIG_FILE
+    echo "$CONFIG_FILE has been created for your convenience."
+fi
+
+echo "=== $CONFIG_FILE contents ==="
+cat "$CONFIG_FILE"
+sudo service go-to-bed stop
+sudo service go-to-bed start
+
+update-rc.d go-to-bed defaults
