a5c5c02a5325d310d4b82a76ee328becd06160d5
Eugene R. Miller <theerm@gmail.com>
2013-08-04 23:59:58 -0600

    Updated roadmap

--- python/update.sh
+++ python/update.sh
@@ -3,7 +3,7 @@
 git pull
 sudo cp -v ./go-to-bed-daemon.py /usr/bin/
 sudo cp -v ./go-to-bed.py /usr/bin/
-sudo cp -v ./run-go-to-bed-as-user.sh /usr/bin/
+sudo cp -v ./go-to-bed-init.sh /etc/init.d/go-to-bed
 sudo chmod +x /usr/bin/go-to-bed-daemon.py -c
 sudo chmod +x /usr/bin/go-to-bed.py -c
 sudo chmod +x /etc/init.d/go-to-bed -c
@@ -13,10 +13,11 @@ CONFIG_FILE="/etc/go-to-bed.conf"
 
 if [ ! -f "$CONFIG_FILE" ]
 then
-    sudo echo "USERS=\"\"" >> $CONFIG_FILE
-    sudo echo "# example:USERS=\"child1login,child2login\" #separate by ," >> $CONFIG_FILE
+    echo "Generating $CONFIG_FILE"
+    sudo echo "USERS=\"\"" | sudo tee -a $CONFIG_FILE
+    sudo echo "# example:USERS=\"child1login,child2login\" #separate by ," | sudo tee -a $CONFIG_FILE
     
-    sudo echo "URL=\"http://localhost/go-to-bed/\"\n" >> $CONFIG_FILE
+    sudo echo -e "URL=\"http://localhost/go-to-bed/\"\n" | sudo tee -a $CONFIG_FILE
     echo "$CONFIG_FILE has been created for your convenience."
 fi
 
