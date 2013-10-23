0c413873905c7722e5e05e6c1e347c3f4ce00c1e
Eugene R. Miller <theerm@gmail.com>
2013-10-23 07:52:07 -0600

    added cron job

---
+++ python/go-to-bed-cron.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+import subprocess
+import pprint
+import re
+
+pp = pprint.PrettyPrinter(indent=4)
+session_re = re.compile("(Session[0-9]+)\:")
+var_val_re = re.compile("\t(.*)\s\=\s\'(.*)\'")
+var_val_bool_re = re.compile("\t(.*)\s\=\s(TRUE|FALSE)")
+ps_re = re.compile("(\d+)\s(.*?)\s+(.*)")
+ensure_for = ['sam', 'halle', 'elijah']
+URL = "http://localhost/go-to-bed/"
+
+def exe(cmd, shell=False):
+    try:
+        return subprocess.check_output(cmd, shell=shell).strip()
+    except subprocess.CalledProcessError, e:
+        logger.error("subprocess.CalledProcessError %s %s" % (e, cmd))
+    return ""
+
+def parse_etc_passwd():
+    fp = open("/etc/passwd","r")
+    user_map = {}
+    for l in fp:
+        parts = l.split( ":" )
+        user_map[parts[2]] = parts[0]
+
+    return user_map
+
+def parse_ck_list_sessions():
+    users = parse_etc_passwd()
+    try:
+        output = exe(['ck-list-sessions'])
+    except:
+        return {}
+    lines = output.split("\n")
+    session_name = ""
+    sessions = {}
+    for l in lines:
+        match_session = session_re.match(l)
+        if match_session:
+            session_name = match_session.group(1)
+            sessions[session_name] = {}
+            continue
+        match = var_val_re.search(l)
+        is_bool = False
+        if match is None:
+            match = var_val_bool_re.search(l)
+            if match is not None:
+                is_bool = True
+        if match is None:
+            continue
+        key = match.group(1)
+        value = match.group(2)
+        if is_bool:
+            if value == "TRUE":
+                value = True
+            if value == "FALSE":
+                value = False
+
+        if key == 'unix-user':
+            key = 'unix-uid'
+
+        sessions[session_name][key] = value
+
+        if key == 'unix-uid':
+            sessions[session_name]['unix-user'] = users[value]
+
+    return sessions
+
+xsessions = parse_ck_list_sessions()
+dms = [
+    '/etc/init.d/kdm',
+    '/etc/init.d/lightdm',
+    '/etc/init.d/gdm'
+]
+for s in xsessions:
+    if s['unix-user'] in ('sam', 'halle'):
+        for dm in dms:
+            if os.path.exists(dm):
+                basename = os.path.basename(dm)
+                is_running = exe("ps -Af | grep %s | grep -v grep" % (basename,), shell=True)
+                if is_running:
+                    exe([dm, "restart"])
